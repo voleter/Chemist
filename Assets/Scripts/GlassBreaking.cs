8cc6c077ebbf79e6e7c88856c72d87e71394fc7c
voleter <sayyegh@gmail.com>
2024-01-17 17:25:58 +0300

    Sounds and Scripts

---
+++ Assets/Scripts/GlassBreaking.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SoldierCollision : MonoBehaviour
+{
+    public AudioClip collisionSound; 
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.clip = collisionSound;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "Floor") 
+        {
+            audioSource.Play();
+        }
+    }
+}
